501e6431f4660fa37671650d2b3f3eb444d295c2
白帶豬排 <>
2020-09-27 23:44:35 +0800

    Socket simple component

---
+++ unity-playground/Assets/2 Socket/Client.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System;
+using System.Text;
+
+
+public class Client : MonoBehaviour
+{    
+    Socket socket;
+    byte[] buffer = new byte[1024];
+
+    void Start()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect("127.0.0.1", 6666);
+        StartReceive();
+        Send();
+
+    }
+
+    void StartReceive()
+    {
+        socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, null);
+    }
+
+    void ReceiveCallback(IAsyncResult res)
+    {
+        // If length of result is 0, we return
+        int len = socket.EndReceive(res);
+
+        if (len == 0)
+            return;
+
+        string str = Encoding.UTF8.GetString(buffer, 0, len);
+        print(str);
+
+        StartReceive();
+
+    }
+    void Send()
+    {
+        socket.Send(Encoding.UTF8.GetBytes("身寸"));
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+            Send();
+
+    }
+}
---
+++ unity-playground/Assets/2 Socket/Server.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+
+
+public class Server : MonoBehaviour
+{
+    Socket socket;
+    byte[] buffer = new byte[1024];
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Bind(new IPEndPoint(IPAddress.Any, 6666));
+        socket.Listen(0);
+
+        StartAccept();
+    }
+
+    void StartAccept()
+    {
+        print("StartAccept");
+
+        socket.BeginAccept(AcceptCallback, null);
+    }
+    void AcceptCallback(IAsyncResult res)
+    {
+        print("AcceptCallback");
+
+        Socket client = socket.EndAccept(res);
+ 
+        StartReceive(client);
+        StartAccept();
+
+    }
+    void StartReceive(Socket client)
+    {
+        print("StartReceive");
+
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, client);
+    }
+    void ReceiveCallback(IAsyncResult res)
+    {
+        print("ReceiveCallback");
+
+        Socket client = res.AsyncState as Socket;
+        int len = client.EndReceive(res);
+        if (len == 0)
+            return;
+
+        string str = Encoding.UTF8.GetString(buffer, 0, len);
+        print(str);
+        StartReceive(client);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
